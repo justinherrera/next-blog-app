719ab09f8a4e18817e8d489d5c47ccd4905e337c
Justin <justinherrera013@yahoo.com>
2024-06-15 15:02:54 +0800

    working sign in with google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import { SignIn } from "./_components/sign-in"
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div>
+      Hello World
+      <SignIn />
+    </div>
   );
 }
