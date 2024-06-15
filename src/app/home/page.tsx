3d7f3f1f6651f948b7060ea07b7aa3c72ae813ca
Justin <justinherrera013@yahoo.com>
2024-06-15 15:53:21 +0800

    added getting session

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,14 @@
 import SignOut from "../../app/_components/sign-out"
+import { auth } from "../../../auth"
+import { redirect } from "next/navigation"
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+  console.log(session)
+  if (!session?.user) {
+    redirect("/")
+  }
+
   return (
     <div>
       Authorized Home
