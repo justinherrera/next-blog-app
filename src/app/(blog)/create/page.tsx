aa3b51b1de87e01ad756e0d4b9a6f3a95ef079b5
Justin <justinherrera013@yahoo.com>
2024-06-23 20:08:28 +0800

    fixed create form field validation

--- src/app/(blog)/create/page.tsx
+++ src/app/(blog)/create/page.tsx
@@ -5,22 +5,102 @@ import BlogCreateTitleInput from "@/app/_components/blogs/create/blog-create-tit
 import BlogCateogriesList from "@/app/_components/blogs/create/blog-categories-list"
 
 import { Suspense } from "react"
+import CreateForm from "@/app/_components/blogs/create/create-form"
+import { z } from 'zod'
+
+const acceptedFileTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp']
+const schema = z.object({
+  title: z.string({
+    required_error: "Title is required",
+  }).min(10, { message: "Must be 10 or more characters long" }),
+  content: z.string().min(50),
+  category: z.string(),
+  image: z.object({
+    size: z.number(),
+    type: z.string(),
+    lastModified: z.number(),
+  })
+  // .refine(file => file.size === 0, { message: "Please upload an image" })
+  .refine(file => file.size < 1024 * 1024 * 10, { message: "File size must be less than 10MB" })
+  .refine(file => acceptedFileTypes.includes(file.type), { message: "Please upload a valid image" })
+})
 
 
-export default function Create() {
+
+type Fields = {
+  title: string;
+  content: string;
+  category: string;
+  image: {
+    size: number;
+    type: string;
+    lastModified: number;
+  };
+}
+ 
+type FormState = {
+  message: string;
+  errors: Record<keyof Fields, string[]> | undefined;
+  fieldValues?: Fields
+}
+
+export default async function Create() {
+  const response = await fetch(`http://localhost:3000/api/categories`)
+  const { categories } = await response.json()
+
+  async function createPost(currentState: FormState, formData: FormData): Promise<FormState> {
+    'use server'
+
+    const { title, content, category, image } = Object.fromEntries(formData)
+    const validatedFields = schema.safeParse({
+      title,
+      content,
+      category,
+      image,
+    })
+   
+    
+    if (!validatedFields.success) {
+      return {
+        message: "error",
+        errors: {
+          title: validatedFields.error.flatten().fieldErrors.title as string[],
+          content: validatedFields.error.flatten().fieldErrors.content as string[],
+          category: validatedFields.error.flatten().fieldErrors.category as string[],
+          image: validatedFields.error.flatten().fieldErrors.image as string[],
+        },
+
+      }
+    }
+
+    return {
+      fieldValues: {
+        title,
+        content,
+        category,
+        image
+      }
+    }
+
+
+  }
 
 
   return (
   <div className="mt-24 w-full  flex items-center justify-center">
     <div>
-      <BlogCreateTitleInput />
+      {/* <BlogCreateTitleInput />
       <Tiptap />
       <Suspense fallback={<p>Loading...</p>}>
         <BlogCateogriesList /> 
       </Suspense>
-      <BlogCreateImageUpload />
+      <BlogCreateImageUpload /> */}
+      <Suspense fallback={<p>Loading...</p>}>
+        <CreateForm categories={categories} createPost={createPost} />
+      </Suspense>
+      
           
-      <button className="mt-4 bg-black text-white py-1 px-4 rounded-2xl">Publish</button>
+      {/* <button className="mt-4 bg-black text-white py-1 px-4 rounded-2xl">Publish</button> */}
     </div>
 
 
---
+++ src/app/_components/blogs/create/create-form.tsx
@@ -0,0 +1,193 @@
+"use client"
+
+import { useEditor, EditorContent } from '@tiptap/react'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import { Italic as TiptapItalic } from '@tiptap/extension-italic'
+import Strike from '@tiptap/extension-strike'
+import Highlight from '@tiptap/extension-highlight'
+import StarterKit from '@tiptap/starter-kit'
+import { Code as TiptapCode } from '@tiptap/extension-code'
+import Blockquote from '@tiptap/extension-blockquote'
+import { Underline as TiptapUnderline } from '@tiptap/extension-underline'
+import BulletList from '@tiptap/extension-bullet-list'
+import ListItem from '@tiptap/extension-list-item'
+import CodeBlock from '@tiptap/extension-code-block'
+import Heading from '@tiptap/extension-heading'
+import { ArrowDownUp } from "lucide-react"
+
+import BlogCreateToolbar from '../create/blog-create-toolbar'
+import BlogCategoriesList from './blog-categories-list'
+import { Category } from '@/app/lib/definitions'
+import BlogCreateCategorySelect from './blog-create-category-select'
+import { useState } from 'react'
+
+import { Upload } from "lucide-react"
+
+import { useFormState } from "react-dom";
+
+type Fields = {
+  title: string;
+  content: string;
+  category: string;
+  image: number;
+}
+ 
+type FormState = {
+  message: string;
+  errors: Record<keyof Fields, string[]> | undefined;
+  fieldValues?: Fields
+}
+
+export default function CreateForm({ createPost, categories }: { createPost: FormData, categories: Category[] })  {
+  const [image, setImage] = useState<string | null>(null)
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
+  const [editorContent, setEditorContent] = useState("");
+
+  const [state, formAction] = useFormState<FormState, FormState>(createPost, {
+    message: "",
+    errors: undefined,
+    fieldValues: {
+      title: "",
+      content: "",
+      category: "",
+      image: "",
+    }
+  });
+  
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Document,
+      Paragraph,
+      Text,
+      TiptapCode,
+      Highlight.configure({ multicolor: true }),
+      TiptapItalic,
+      Strike,
+      TiptapUnderline,
+      Blockquote,
+      BulletList, 
+      ListItem,
+      CodeBlock,
+      Heading.configure({
+        levels: [1, 2, 3],
+      }),
+    ],
+    editorProps: {
+      attributes: {
+        class: "w-full px-4 border border-gray-300 py-2 rounded border-t-0 rounded-t-none focus:outline-none h-56 shadow-lg bg-white"
+      }
+    },
+    onUpdate({ editor }) {
+      setEditorContent(editor.getHTML());
+    },
+    content: 'Content goes here...',
+  })
+
+  if (!editor) {
+    return null
+  }
+
+  console.log(state)
+  return (
+    <div>
+      <p>
+        errors:
+        {/* {state} */}
+      </p>
+      <form action={formAction}>
+        <div className="mt-2">
+
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="block w-full py-1.5 text-gray-900 shadow-sm ring-gray-300 placeholder:text-gray-400 px-2 font-bold text-4xl focus:outline-none"
+            placeholder="Your title goes here..."
+            autoFocus
+          />
+          <span className="text-sm text-red-500">{state?.errors?.title}</span>
+        </div>
+
+        <div>
+          <div className="flex flex-col ">
+            <div className="mt-2 flex space-x-8 border border-gray-300 px-4 rounded rounded-b-none">
+              <BlogCreateToolbar editor={editor}/>
+            </div>
+          </div>
+          <input type="hidden" name="content" value={editorContent} />
+          <EditorContent editor={editor} name="content" />
+          <span className="text-sm text-red-500">{state?.errors?.content}</span>
+        </div>
+
+        <div>
+          {
+            selectedCategory ? (
+              <select name="category" className="hidden">
+                <option value={selectedCategory}>{selectedCategory}</option>
+              </select>
+            ) : ""
+          }
+          
+          {/* <BlogCategoriesList /> */}
+          {/* <BlogCreateCategorySelect categories={categories} /> */}
+          <p className="font-bold mt-4">Choose a category:</p>
+            <div 
+              className="border p-2 px-4 rounded-lg border-gray-300 my-2 w-[50%] flex justify-between cursor-pointer shadow-lg"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <p>{(!selectedCategory) ? "Travel" : selectedCategory}</p>
+              <ArrowDownUp className="h-5 w-5 pt-1 text-gray-300" />
+            </div>
+            <span className="text-sm text-red-500">{state?.errors?.category}</span>
+            {
+              isOpen ? (
+                <div className="border border-gray-300 my-2 w-[50%] flex-col">
+                  {
+                    categories.map((category: Category) => (
+                      <div 
+                        key={category.id} 
+                        className={`w-full border-gray-300 cursor-pointer ${(selectedCategory === category.name) ? "bg-black text-white" : "hover:bg-gray-300"} `}
+                        onClick={() => {
+                          setIsOpen(false)
+                          setSelectedCategory(category.name)
+                        }}
+                      >
+                        <p className="p-2 ">{category.name}</p>
+                      </div>
+                    ))
+                  }
+                </div>
+              ) : ""
+            }
+          </div>
+
+        <div className="flex flex-col w-full mt-4 bg-grey-lighter">
+          <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide border border-blue cursor-pointer hover:bg-blue hover:text-gray-400">
+              <Upload />
+              <span className="mt-2 text-base leading-normal">{(!image) ? "Add cover image" : image}</span>
+              <input 
+                type='file' 
+                className="hidden" 
+                accept="image/*"         
+                name="image"
+                onChange={(e) => {
+                  if (e.target.files) {
+                    setImage(e.target.files[0].name)
+                  }
+                  
+                }} />
+              
+          </label>
+          <span className="text-sm text-red-500">{state?.errors?.image}</span>
+        </div>
+
+        <button type="submit" className="mt-4 bg-black text-white py-1 px-4 rounded-2xl">Publish</button>
+      </form>
+      
+    </div>
+  )
+}
