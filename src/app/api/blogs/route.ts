597a4943734962308143ceea81d3f4380c64a1b3
Justin <justinherrera013@yahoo.com>
2024-06-26 23:09:27 +0800

    added a wip profile page

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -3,8 +3,11 @@ import prisma from "@/app/utils/prisma-connect"
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const slug = searchParams.get('slug') as string
+  const userId = searchParams.get('userId') as string
 
-  if (!slug) {
+  console.log(`searchParams ${userId}`)
+
+  if (!slug && !userId) {
     const posts = await prisma.post.findMany({
       orderBy: [
         {
@@ -16,6 +19,24 @@ export async function GET(request: Request) {
         user: true
       }
     })
+    await prisma.$disconnect()
+    return Response.json({ posts })
+  }
+
+  if (userId) {
+    console.log("called???")
+    const posts = await prisma.post.findMany({
+      where: {
+        user: {
+          id: userId
+        }
+      },
+      include: {
+        user: true,
+        category: true
+      }
+    })
+
     await prisma.$disconnect()
     return Response.json({ posts })
   }
