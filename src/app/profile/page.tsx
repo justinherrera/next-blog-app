c2a4e20e1e8376d384f46ba4daf10c63a9610833
Justin <justinherrera013@yahoo.com>
2024-06-27 22:29:18 +0800

    fixed post title in profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -12,7 +12,7 @@ export default async function Profile() {
   const { user } = session
   
 
-  const response = await fetch(`http://localhost:3000/api/blogs?userId=${user?.id}`)
+  const response = await fetch(`http://localhost:3000/api/blogs?userId=${user?.id}`, { cache: 'no-store' })
   const { posts } = await response.json()
 
   return (
@@ -25,64 +25,46 @@ export default async function Profile() {
           </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
+          {posts.map((post: Post) => (
             <article key={post.id} className="flex flex-col items-start justify-between">
               <div className="relative w-full">
-                <Link href={`/${post.slug}`}>
+                <Image
+                  src={post.imageUrl}
+                  height={500}
+                  width={500}
+                  alt=""
+                  className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                />
+                {/* <Link href={`/${post.slug}`}>
                   <Image
                     src={post.imageUrl}
                     height={500}
                     width={500}
                     alt=""
                     className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                  /></Link>
-                {/* <Image
-                  src={post.imageUrl}
-                  height={500}
-                  width={500}
-                  alt=""
-                  className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                /> */}
-                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                  /></Link> */}
+                {/* <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" /> */}
               </div>
               <div className="max-w-xl">
                 <div className="mt-4 flex items-center text-xs">
-                  {/* <time dateTime={post.datetime} className="text-gray-500">
-                    {post.date}
-                  </time> */}
-                  <a
+                  {/* <a
                     href="#"
                     className="relative z-10 rounded-full bg-gray-50 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
                   >
                     {post.category.name}
-                  </a>
+                  </a> */}
                 </div>
                 <div className="">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  {/* <span className="">{post.title}</span> */}
+                  <Link className="text-lg font-semibold" href={`/${post.slug}`}>{post.title}</Link>
+                  {/* <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                     <a href={`/${post.slug}`}>
-                      <span className="absolute inset-0" />
-                      {post.title}
+                      <span className="absolute inset-0">{post.title}</span>
+                      
                     </a>
-                  </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+                  </h3> */}
+                  <p className="mt-5 text-sm leading-6 text-gray-600">{post.description}</p>
                 </div>
-                {/* <div className="relative mt-8 flex items-center gap-x-4">
-                  <Image 
-                    src={post.user.image} 
-                    height={100}
-                    width={100}
-                    alt="" 
-                    className="h-10 w-10 rounded-full bg-gray-100" />
-                  <div className="text-sm leading-6">
-                    <p className="font-semibold text-gray-900">
-                      <a href="#">
-                        <span className="absolute inset-0" />
-                        {post.user.name}
-                      </a>
-                    </p>
-                    <p className="text-gray-600">{post.user.role}</p>
-                  </div>
-                </div> */}
               </div>
             </article>
           ))}
