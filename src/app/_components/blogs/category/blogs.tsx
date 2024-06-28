92bf7cdef035f04917f2d6959ffdeea90a18126a
Justin <justinherrera013@yahoo.com>
2024-06-29 03:23:51 +0800

    fixed text wrapping of title element

--- src/app/_components/blogs/category/blogs.tsx
+++ src/app/_components/blogs/category/blogs.tsx
@@ -17,7 +17,7 @@ export default async function Blogs({ posts, category }: { posts: Post[], catego
       <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         {posts.map((post: Post) => (
           <article key={post.id} className="flex flex-col items-start justify-between">
-            <div className="relative w-full">
+            <div className="w-full">
               <Image
                 src={post.imageUrl}
                 height={500}
@@ -25,34 +25,10 @@ export default async function Blogs({ posts, category }: { posts: Post[], catego
                 alt=""
                 className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
               />
-              {/* <Link href={`/${post.slug}`}>
-                <Image
-                  src={post.imageUrl}
-                  height={500}
-                  width={500}
-                  alt=""
-                  className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
-                /></Link> */}
-              {/* <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" /> */}
             </div>
-            <div className="max-w-xl">
-              <div className="mt-4 flex items-center text-xs">
-                {/* <a
-                  href="#"
-                  className="relative z-10 rounded-full bg-gray-50 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                >
-                  {post.category.name}
-                </a> */}
-              </div>
-              <div className="">
-                {/* <span className="">{post.title}</span> */}
+            <div className="w-[24rem] text-pretty break-words">
+              <div className="w-full mt-4">
                 <Link className="text-lg font-semibold" href={`/${post.slug}`}>{post.title}</Link>
-                {/* <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href={`/${post.slug}`}>
-                    <span className="absolute inset-0">{post.title}</span>
-                    
-                  </a>
-                </h3> */}
                 <p className="mt-5 text-sm leading-6 text-gray-600">{post.content.substring(0, 200)}...</p>
               </div>
             </div>
