64f283a27c26e235f954f68d9d05e18abd3986b5
Justin <justinherrera013@yahoo.com>
2024-06-29 23:15:35 +0800

    fixed google button to be clicked within the whole button

--- src/app/_components/auth/google-button.tsx
+++ src/app/_components/auth/google-button.tsx
@@ -3,8 +3,12 @@ import { signIn } from "../../../../auth"
 export default function GoogleButton() {
   return (
     <div className="mt-6 grid gap-4">
-      <a
-        href="#"
+      <form action={async () => {
+          "use server"
+          await signIn("google", { redirectTo: "/feed"})
+          }}>
+      <button
+        type="submit"
         className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
       >
         <svg className="h-5 w-5" aria-hidden="true" viewBox="0 0 24 24">
@@ -25,14 +29,16 @@ export default function GoogleButton() {
             fill="#34A853"
           />
         </svg>
-        <form action={async () => {
+        <span className="text-sm font-semibold leading-6">Google</span>
+        {/* <form action={async () => {
           "use server"
           await signIn("google", { redirectTo: "/feed"})
           }}
         >
-          <button type="submit" className="text-sm font-semibold leading-6">Google</button>
-        </form>
-      </a>
+          <button type="submit" className="text-sm font-semibold leading-6">Google</button> */}
+        
+      </button>
+      </form>
    </div>
   )
 }
