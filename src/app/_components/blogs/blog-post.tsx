9ed8f63e3d88a4bcd38d0762d24089e0690e609a
Justin <justinherrera013@yahoo.com>
2024-07-02 20:06:24 +0800

    fixed alignments in slug page for default screen

--- src/app/_components/blogs/blog-post.tsx
+++ src/app/_components/blogs/blog-post.tsx
@@ -17,7 +17,7 @@ export default function BlogPost({ post }: { post: Post }) {
   const [isLiked, setIsLiked] = useState(false)
 
   return (
-    <div className="bg-white px-6 py-32 lg:px-8">
+    <div className="bg-white px-6 py-16 sm:py-32 lg:px-8">
       <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
         <p className="text-base font-semibold leading-7 text-indigo-600">Introducing ✨</p>
         <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{post.title}</h1>
@@ -58,7 +58,7 @@ export default function BlogPost({ post }: { post: Post }) {
             Faucibus commodo massa rhoncus, volutpat.
           </figcaption>
         </figure>
-        <div className="mt-16 max-w-2xl">
+        <div className="mt-8 sm:mt-16 max-w-2xl">
           {/* <p>{post.content}</p> */}
           {parse(post.content)}
 
