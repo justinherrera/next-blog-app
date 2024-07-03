533ce1f73c0d28ba619271e128e0d51f1757c84f
Justin <justinherrera013@yahoo.com>
2024-07-03 22:58:15 +0800

    added a not found component when a slug post is not found

--- src/app/(blog)/[slug]/page.tsx
+++ src/app/(blog)/[slug]/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/app/utils/prisma-connect"
 import BlogPost from "@/app/_components/blogs/blog-post"
 import { Suspense } from "react"
 import LoadingSlug from "@/app/_components/blogs/skeletons/loading-slug"
+import NotFound from "@/app/not-found"
 
 export default async function Page(
   {
@@ -16,6 +17,8 @@ export default async function Page(
   
   const response = await fetch(`${process.env.BASE_URL}/api/blogs?slug=${params.slug}`, { next: { revalidate: 3600 } })
   const { post } = await response.json()
+  
+  if (!post) return <NotFound />
 
   return (
     <Suspense fallback={<LoadingSlug />}>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -6,12 +8,12 @@ export default function NotFound() {
       <h1 className="mt-4 text-3xl font-bold text-gray-900 sm:text-5xl">Page not found</h1>
       <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
       <div className="mt-10 flex items-center justify-center gap-x-6">
-        <a
-          href="#"
+        <Link
+          href="/feed"
           className=" bg-black px-3.5 py-2.5 text-sm font-semibold text-white rounded-2xl"
         >
           Go back home
-        </a>
+        </Link>
       </div>
     </div>
   </main>
