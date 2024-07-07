b64099c81b9c499183d9d9d73bde1b8186e927ae
Justin <justinherrera013@yahoo.com>
2024-07-07 16:56:05 +0800

    added delete confirm modal

---
+++ src/app/_components/blogs/slug/delete-dialog.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { Toaster, toast } from 'sonner'
+
+export default function DeleteDialog({ postId, setIsDeleting }: { postId: number, setIsDeleting: React.Dispatch<React.SetStateAction<boolean>> }) {
+  const router = useRouter()
+
+  const handleDelete = async () => {
+    setIsDeleting(false)
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?postId=${postId}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      router.push('/feed')
+      router.refresh()
+    }
+  }
+
+  return (
+    <div className="bg-white fixed top-[40%] mx-12 lg:left-[30%] md:left-[20%] sm:left-[10%] 2xl:top-[40%] 2xl:left-[40%] p-8 shadow-lg border rounded-lg">
+      <span className="font-bold">Are you sure you want to delete this post?</span>
+      <div className="flex justify-end space-x-4 mt-4">
+        <button className="text-sm font-medium" onClick={() => setIsDeleting(false)}>Cancel</button>
+        <button className="rounded border py-2 px-4 bg-black text-white text-sm font-medium" onClick={handleDelete}>Continue</button>
+      </div>
+    </div>
+  )
+}
--- src/app/_components/blogs/slug/edit-modal.tsx
+++ src/app/_components/blogs/slug/edit-modal.tsx
@@ -1,22 +1,6 @@
 import { FilePenLine, Trash } from "lucide-react"
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-import { useRouter } from "next/navigation";
 
-export default function EditModal({ postId }: { postId: number }) {
-  const router = useRouter()
-
-  const handleDelete = async () => {
-
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs?postId=${postId}`, {
-      method: "DELETE",
-    });
-
-    console.log(response)
-    router.push('/feed')
-
-  }
-
+export default function EditModal({ postId, setIsDeleting }: { postId: number, setIsDeleting: React.Dispatch<React.SetStateAction<boolean>>}) {
 
   return (
     <div className="border border-gray-300 absolute top-7 right-2 w-44 rounded bg-white shadow-lg">
@@ -24,7 +8,7 @@ export default function EditModal({ postId }: { postId: number }) {
         <FilePenLine className="w-4 h-4" />
         <span className="ml-2">Edit</span>
       </button>
-      <button className="py-2 px-2 cursor-pointer hover:bg-gray-300 w-full text-left text-red-500 text-sm flex items-center" onClick={handleDelete}>
+      <button className="py-2 px-2 cursor-pointer hover:bg-gray-300 w-full text-left text-red-500 text-sm flex items-center" onClick={() => setIsDeleting(true)}>
       <Trash className="w-4 h-4" />
       <span className="ml-2">Delete</span>
     </button>
