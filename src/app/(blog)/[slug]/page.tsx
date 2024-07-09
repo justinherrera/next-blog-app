25fc78b3f68058d8cc166165ade2855f7e7af8ad
Justin <justinherrera013@yahoo.com>
2024-07-09 19:47:40 +0800

    clean up slug page comments and unused imports

--- src/app/(blog)/[slug]/page.tsx
+++ src/app/(blog)/[slug]/page.tsx
@@ -1,16 +1,6 @@
 
-
-import prisma from "@/app/utils/prisma-connect"
-import SlugPost from "@/app/_components/blogs/slug/slug-post"
-import { Suspense, useEffect, useState } from "react"
-import LoadingSlug from "@/app/_components/blogs/skeletons/loading-slug"
-import NotFound from "@/app/not-found"
-import DeleteDialog from "@/app/_components/blogs/slug/delete-dialog"
-import { Post, User } from "@/app/lib/definitions"
+import { User } from "@/app/lib/definitions"
 import { auth } from "../../../../auth"
-import { useSession } from "next-auth/react"
-import { BaseNextResponse } from "next/dist/server/base-http"
-import { Toaster } from "sonner"
 import SlugPage from "@/app/_components/blogs/slug/slug-page"
 
 export default async function Page(
@@ -29,19 +19,6 @@ export default async function Page(
 
 
   return (
-    // <div className="">
-    //   <Toaster position="top-right" richColors  />
-    //   <div className={`${isDeleting ? "brightness-50" : ""}`}>
-    //     <Suspense fallback={<LoadingSlug />}>
-    //       <SlugPost post={post} setIsDeleting={setIsDeleting} isDeleting={isDeleting} />
-    //     </Suspense>
-    //   </div>
-
-
-    //   {
-    //     isDeleting ? <DeleteDialog postId={post.id} setIsDeleting={setIsDeleting} /> : ""
-    //   }
-    // </div>
     <SlugPage slug={params.slug} user={user as User} />
 
     
