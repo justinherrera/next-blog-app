c9d6cc6c7581cf44221e836491450a77960b5af6
Justin <justinherrera013@yahoo.com>
2024-07-16 02:27:36 +0800

    removed unused comment

--- src/app/_components/blogs/feed/blogs.tsx
+++ src/app/_components/blogs/feed/blogs.tsx
@@ -4,8 +4,6 @@ import CategoriesNavigation from "./categories-navigation"
 import { getPosts } from "@/app/lib/actions"
 
 export default async function Blogs() {
-  // const response = await fetch(`${process.env.BASE_URL}/api/blogs?offset=0&limit=2`, { cache: 'no-store' })
-  // const { posts } = await response.json()
 
   const { posts } = await getPosts(0, 2)
 
