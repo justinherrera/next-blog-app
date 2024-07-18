bc4f4060cc144e4700baf202f424ed39a8b76ced
Justin <justinherrera013@yahoo.com>
2024-07-18 16:54:09 +0800

    added a link hover in post title in feed page

--- src/components/blogs/feed/blog-row-content.tsx
+++ src/components/blogs/feed/blog-row-content.tsx
@@ -18,7 +18,7 @@ export default function BlogRowContent({ title, content, slug }: { title: string
 
   return (  
     <div className="sm:h-auto md:h-auto lg:h-[62%] sm:full lg:w-full text-wrap break-all overflow-hidden">
-      <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600 line-clamp-1">
+      <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600 line-clamp-1 hover:text-gray-500">
         <Link href={`/${slug}`}>{title}</Link>
       </h3>
       <p className="mt-5 text-sm leading-6 text-gray-600 line-clamp-4">{content.replace(/<[^>]+>/g, '')}</p>
