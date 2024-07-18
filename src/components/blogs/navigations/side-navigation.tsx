44b18695e018addcbfe5674b98fc4527a1a19772
Justin <justinherrera013@yahoo.com>
2024-07-19 02:31:07 +0800

    removed displaying latest posts

--- src/components/blogs/navigations/side-navigation.tsx
+++ src/components/blogs/navigations/side-navigation.tsx
@@ -1,26 +1,14 @@
 import { Post } from "@/lib/definitions"
 import Link from "next/link"
+import TrendingPostsList from "@/components/blogs/navigations/trending-posts-list"
 
-export default async function SideNavigation({ trendingPosts, latestPosts }: { trendingPosts: Post[], latestPosts: Post[] }) {
+export default async function SideNavigation({ trendingPosts }: { trendingPosts: Post[] }) {
 
   return (
     <div className={`${!trendingPosts ? "h-screen" : ""} sticky flex flex-col top-0 pt-12`}>
-          {/* <p>Authors</p> */}
-      <div className="w-full mb-4">
-        <div className="flex space-x-2">
-          <p className="font-bold text-2xl py-2 px-4">Trending Posts 🔥</p>
-        </div>
-        {
-          trendingPosts.map((post) => (
-            <div key={post.id} className="flex flex-col p-4 border-b border-gray-200 hover:bg-gray-100">
-              <Link href={`/${post.slug}`}>{post.title}</Link>
-            </div>
-          ))
-        }
+      <TrendingPostsList trendingPosts={trendingPosts} />
 
-      </div>
-
-      <div className="w-full my-4">
+      {/* <div className="w-full my-4">
         <div className="flex space-x-2">
           <p className="font-bold text-2xl py-2 px-4">Latest Posts ✨</p>
         </div>
@@ -32,7 +20,7 @@ export default async function SideNavigation({ trendingPosts, latestPosts }: { t
           ))
         }
 
-      </div>
+      </div> */}
       
       {/* <FeaturedPost post={post} /> */}
   </div>
