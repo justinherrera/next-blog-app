f526f894b4a988a5f84764e4588d179a05c6a736
Justin <justinherrera013@yahoo.com>
2024-07-18 15:34:54 +0800

    added line-clamp in blog content to add ... at the end of the displayed text

--- src/components/blogs/feed/blog-row-content.tsx
+++ src/components/blogs/feed/blog-row-content.tsx
@@ -1,15 +1,27 @@
 import Link from "next/link"
 
 
+const extractContent = (text: string) => {
+  const content = text.match(/[^\.!\?]+[\.!\?]+/g);
+  if (content && content.length >= 2) {
+    return content[0] + " " + content[1] + content[2];
+  } else if (content && content.length === 1) {
+    return content[0];
+  } else {
+    return ""; 
+  }
+}
+
 export default function BlogRowContent({ title, content, slug }: { title: string, content: string, slug: string }) {
+
+
 
   return (  
     <div className="sm:h-auto md:h-auto lg:h-[62%] sm:full lg:w-full text-wrap break-all overflow-hidden">
-      <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+      <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600 line-clamp-1">
         <Link href={`/${slug}`}>{title}</Link>
       </h3>
-      {/* <p className="mt-5 text-sm leading-6 text-gray-600" dangerouslySetInnerHTML={{ __html: content.substring(0, 200) }}></p> */}
-      <p className="mt-5 text-sm leading-6 text-gray-600">{content.substring(0, 300).replace(/<[^>]+>/g, '')}</p>
+      <p className="mt-5 text-sm leading-6 text-gray-600 line-clamp-4">{content.replace(/<[^>]+>/g, '')}</p>
     </div>
   )
 }
