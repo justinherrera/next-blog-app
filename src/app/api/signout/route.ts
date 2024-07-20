0738f7dbf83bc7b56d29b7f306c9e8496db17076
Justin <justinherrera013@yahoo.com>
2024-07-21 00:42:48 +0800

    removed comments

--- src/app/api/signout/route.ts
+++ src/app/api/signout/route.ts
@@ -3,8 +3,6 @@ import { signOut } from "../../../../auth";
 import { redirect } from 'next/navigation'
 
 export async function POST(request: Request) {
-  // await signOut({ redirectTo: "/login", redirect: true });
-  console.log("called")
   await signOut({ redirect: false, redirectTo: "/login" });
   return Response.json({ message: "Signed out" });
 
