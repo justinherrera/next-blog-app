ec4050c33f3e54ffe09473f0b993bae935055b61
Justin <justinherrera013@yahoo.com>
2024-07-31 06:04:37 +0800

    added a modal if guest tries to hit like button then show a message of you are not currently logged in

--- src/components/blogs/slug/slug-post.tsx
+++ src/components/blogs/slug/slug-post.tsx
@@ -24,6 +24,7 @@ import CodeBlock from '@tiptap/extension-code-block'
 import Heading from '@tiptap/extension-heading'
 import { useEditor, EditorContent } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
+import Link from "next/link"
 
 
 export default function BlogPost({ post, isDeleting, setIsDeleting, user }: { post: Post, isDeleting: boolean, setIsDeleting: React.Dispatch<React.SetStateAction<boolean>>, user: User }) {
@@ -31,9 +32,13 @@ export default function BlogPost({ post, isDeleting, setIsDeleting, user }: { po
   const [isLiked, setIsLiked] = useState<boolean>(post.likes?.find(like => like.userId === post.loggedUser?.id) ? true : false)
   const [isEditing, setIsEditing] = useState(false)
   const [totalLikes, setTotalLikes] = useState(post.likes?.length || 0)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const handleLike = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
+    if (!user) {
+      return setIsModalOpen(!isModalOpen)
+    }
     setIsLiked(!isLiked)
     if (isLiked) {
       setTotalLikes((prevTotalLikes) => prevTotalLikes - 1)
@@ -81,73 +86,97 @@ export default function BlogPost({ post, isDeleting, setIsDeleting, user }: { po
     return null
   }
 
+  // return (
+  //   <div>
+  //     You are not currently logged in. Please login to continue.
+  //   </div>
+  // )
+
   return (
-    <div className="bg-white px-6 py-16 lg:py-32 lg:px-8">
-      <div className="mx-auto md:w-full lg:max-w-3xl text-base leading-7 text-gray-700 text-wrap break-words">
-        <div className="w-full flex justify-between relative">
-          {/* {
-            isDeleting ? <DeleteDialog postId={post.id} setIsDeleting={setIsDeleting} /> : ""
-          } */}
-          
-          <p className="text-base font-semibold leading-7 text-indigo-600">Introducing ✨</p>
-          {
-            user?.id === post.userId ? <EllipsisVertical className="w-5 h=5 text-black cursor-pointer" onClick={() => setIsEditing(!isEditing)} /> : ""
-          }
-          {/* <EllipsisVertical className="w-5 h=5 text-black cursor-pointer" onClick={() => setIsEditing(!isEditing)} /> */}
+    <div>
+      <div className={`bg-white px-6 py-16 lg:py-32 lg:px-8 ${isModalOpen ? "brightness-50" : ""}`}>
+        <div className="mx-auto md:w-full lg:max-w-3xl text-base leading-7 text-gray-700 text-wrap break-words ">
+          <div className="w-full flex justify-between relative">
+            {/* {
+              isDeleting ? <DeleteDialog postId={post.id} setIsDeleting={setIsDeleting} /> : ""
+            } */}
+            
+            <p className="text-base font-semibold leading-7 text-indigo-600">Introducing ✨</p>
             {
-              isEditing && !isDeleting ? <EditModal postId={post.id} setIsDeleting={setIsDeleting} slug={post.slug}  /> : ""
+              user?.id === post.userId ? <EllipsisVertical className="w-5 h=5 text-black cursor-pointer" onClick={() => setIsEditing(!isEditing)} /> : ""
             }
+            {/* <EllipsisVertical className="w-5 h=5 text-black cursor-pointer" onClick={() => setIsEditing(!isEditing)} /> */}
+              {
+                isEditing && !isDeleting ? <EditModal postId={post.id} setIsDeleting={setIsDeleting} slug={post.slug}  /> : ""
+              }
+            
+          </div>
           
-        </div>
-        
-        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{post.title}</h1>
-        <div className="flex items-center gap-x-6 my-8 border-b pb-4 border-gray-200">
-          <div className="flex min-w-0 gap-x-4">
-            <Image 
-              className="h-12 w-12 flex-none rounded-full bg-gray-50" 
-              height={100}
-              width={100}
-              src={post.user.image} alt="" />
-            <div className="min-w-0 flex-auto">
-              <div className="flex space-x-4">
-                <p className="text-sm font-semibold leading-6 text-gray-900">{post.user.name}</p>
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{post.title}</h1>
+          <div className="flex items-center gap-x-6 my-8 border-b pb-4 border-gray-200">
+            <div className="flex min-w-0 gap-x-4">
+              <Image 
+                className="h-12 w-12 flex-none rounded-full bg-gray-50" 
+                height={100}
+                width={100}
+                src={post.user.image} alt="" />
+              <div className="min-w-0 flex-auto">
+                <div className="flex space-x-4">
+                  <p className="text-sm font-semibold leading-6 text-gray-900">{post.user.name}</p>
+                </div>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{format(new Date(post.createdAt), "MMM d, yyyy")}</p>
               </div>
-              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{format(new Date(post.createdAt), "MMM d, yyyy")}</p>
             </div>
           </div>
-        </div>
-        <figure className="mt-16">
-          <Image
-            height={1000}
-            width={1000}
-            className="aspect-video rounded-xl bg-gray-50 object-cover"
-            src={post.imageUrl}
-            alt=""
-          />
-          <figcaption className="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
-            {post.category.name}
-            
-          </figcaption>
-        </figure>
-        <div className="mt-8 sm:mt-16 max-w-2xl">
-          {
-            <EditorContent editor={editor} name="content" />
-          }
+          <figure className="mt-16">
+            <Image
+              height={1000}
+              width={1000}
+              className="aspect-video rounded-xl bg-gray-50 object-cover"
+              src={post.imageUrl}
+              alt=""
+            />
+            <figcaption className="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
+              {post.category.name}
+              
+            </figcaption>
+          </figure>
+          <div className="mt-8 sm:mt-16 max-w-2xl">
+            {
+              <EditorContent editor={editor} name="content" />
+            }
 
-        </div>
-        
-        <div className="mt-8">
-          <button 
-            className={`flex space-x-1 border-2 ${isLiked ? "bg-[#FBD1DA] border-[#F8C8CF]" : "border-black"}  shadow-lg  p-1 px-4  rounded-lg items-center justify-center w-auto`}
-            onClick={handleLike}
-            >
-            <Heart color="#E42D54" isLiked={isLiked} />
-            {/* <Heart className="h-4 w-4" color={`${isLiked ? "#E42D54" : "#000000"}`} /> */}
-            <span className="font-bold text-black text-sm">{isLiked ? `${totalLikes <= 0 ? "Liked" : `Liked (${totalLikes})`}` : `${totalLikes <= 0 ? "Like" : `Like (${totalLikes})`}`}</span>
-          </button>
+          </div>
+          
+          <div className="mt-8">
+            <button 
+              className={`flex space-x-1 border-2 ${isLiked ? "bg-[#FBD1DA] border-[#F8C8CF]" : "border-black"}  shadow-lg  p-1 px-4  rounded-lg items-center justify-center w-auto`}
+              onClick={handleLike}
+              >
+              <Heart color="#E42D54" isLiked={isLiked} />
+              {/* <Heart className="h-4 w-4" color={`${isLiked ? "#E42D54" : "#000000"}`} /> */}
+              <span className="font-bold text-black text-sm">{isLiked ? `${totalLikes <= 0 ? "Liked" : `Liked (${totalLikes})`}` : `${totalLikes <= 0 ? "Like" : `Like (${totalLikes})`}`}</span>
+            </button>
+          </div>
+
         </div>
 
       </div>
+      {
+        isModalOpen ? (
+          <div className="bg-white fixed top-[40%] mx-12 lg:left-[30%] md:left-[20%] sm:left-[10%] 2xl:top-[40%] 2xl:left-[40%] p-8 shadow-lg border rounded-lg">
+            <span className="">You are not currently logged in. Please login to continue.</span>
+            <div className="flex justify-end space-x-4 mt-8">
+              <button className="text-sm font-medium" onClick={() => setIsModalOpen(!isModalOpen)}>Cancel</button>
+              <Link href="/login" className="rounded border py-2 px-4 bg-black text-white text-sm font-medium">Login</Link>
+            </div>
+            {/* <p></p>
+            <button className="text-right">Cancel</button> */}
+          </div>
+        ) : ""
+      }
+
     </div>
+    
   )
 }
