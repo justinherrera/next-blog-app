11bbd4a60cf9b48e950c951587261bed9fbbdaf9
Justin <justinherrera013@yahoo.com>
2024-07-31 17:49:03 +0800

    added authorization when accessing create page in guest mode

--- src/app/(blog)/create/page.tsx
+++ src/app/(blog)/create/page.tsx
@@ -1,10 +1,14 @@
 import { Suspense } from "react"
 import CreateForm from "@/components/blogs/create/create-form"
 import { createPost } from "@/lib/actions"
+import { auth } from "../../../../auth"
+import NotAuthorized from "@/components/auth/not-authorized"
 
 export default async function Create() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/categories`)
   const { categories } = await response.json()
+  const session = await auth()
+  if (!session) return <NotAuthorized />
 
   return (
   <div className="mt-24 w-screen  flex items-center justify-center">
